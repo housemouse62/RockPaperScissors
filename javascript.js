b0f441d4006060ad48fa1bce3b51a253f482cd1a
housemouse62 <ryan.skeels@gmail.com>
2025-08-22 23:50:43 +0300

    score tallies to 5 and shows winning message. done for now. see notes at beginning for to-do list.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,18 @@
+/*
+Things to do:
+- Make the UI look better, at least a little bit. Font, 
+    buttons, etc...
+- Turn the different processes into different funtions
+- Turn conditional logic => tertiary 
+- Have messaging say what the computer chose
+- Have messaging disappear on MouseDown so that it's
+    obvious a new round has happened.
+- After the human or computer wins right now the score goes
+    to zero with the message, a bit weird feeling as a player.
+    Fix it!
+*/
+
+
 // Computer Turn
 function getComputerChoice() {
     let ComputerResponse = Math.floor((Math.random() * 100));
@@ -46,12 +61,22 @@ scissors.addEventListener('click', () =>
         computerScore++;
             } else {
         message.textContent = "It's a total tie! Try again."
-            }
+            };
+        if (humanScore === 5) {
+            message.textContent = "Your score is 5! WINNER WINNER!";
+            humanScore = 0;
+            computerScore = 0;
+        } else if (computerScore === 5) {
+            message.textContent = "Computer takes a fifth win. Game...over...Sorry!";
+            humanScore = 0;
+            computerScore = 0;
+        } else {};
+        
         console.log("Human: " + humanScore);
         console.log("Computer: " + computerScore);
 
-        hS.textContent = `Human Score = ${humanScore}`;
-        cS.textContent = `Computer Score = ${computerScore}`;
+        hS.textContent = `Human: ${humanScore}`;
+        cS.textContent = `Computer: ${computerScore}`;
     };
 
 //Scoreboard 
@@ -61,10 +86,10 @@ let computerScore = 0;
 let divScore = document.querySelector("#score");
 
 let hS = document.createElement("p");
-hS.textContent = `Human: = ${humanScore}`;
+hS.textContent = `Human: ${humanScore}`;
 
 let cS = document.createElement("p");
-cS.textContent = `Computer: = ${computerScore}`;
+cS.textContent = `Computer: ${computerScore}`;
 
 divScore.appendChild(hS);
 divScore.appendChild(cS);
@@ -75,16 +100,3 @@ console.log("Computer: " + computerScore)
 //Messaging
 let message = document.createElement("p");
 divScore.appendChild(message);
-    // if (HumanScore > ComputerScore) {
-    //     console.log("YOU WIN!")
-    // } else if (HumanScore < ComputerScore) {
-    //     console.log("You LOSE!")
-    // } else {
-    //     console.log("Tie Game!")
-    // }
-
-
-
-
-
-// playGame();
