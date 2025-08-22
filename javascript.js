87e3b08a298e244c7c6713c96a921958d37b6b9d
housemouse62 <ryan.skeels@gmail.com>
2025-08-22 23:29:47 +0300

    Scoreboard working, messaging working. next is 5 win max.

--- javascript.js
+++ javascript.js
@@ -1,84 +1,88 @@
-//  function getComputerChoice() {
-//     let ComputerResponse = Math.floor((Math.random() * 100));
-//     if (ComputerResponse < 33) {
-//         CChoice = "rock";
-//     } else if (ComputerResponse > 66) {
-//         CChoice = "paper";
-//     } else {
-//     CChoice = "scissors";}
-//     return CChoice;
-//     }
+// Computer Turn
+function getComputerChoice() {
+    let ComputerResponse = Math.floor((Math.random() * 100));
+    if (ComputerResponse < 33) {
+        CChoice = "rock";
+    } else if (ComputerResponse > 66) {
+        CChoice = "paper";
+    } else {
+    CChoice = "scissors";}
+    return CChoice;
+    }
 
-//function getHumanChoice() {
-let HumanResponse;
 let rock = document.querySelector("#rock");
-rock.addEventListener('click', function() {
-    let HumanResponse = 'rock'
-    return HumanResponse;
+rock.addEventListener('click', () => 
+    {playRound(humanChoice = "rock", computerChoice = getComputerChoice())
 });
 
 let paper = document.querySelector("#paper");
-paper.addEventListener('click', function() {
-    let HumanResponse = 'paper'
-    return HumanResponse;
+paper.addEventListener('click', () => 
+    {playRound(humanChoice = "paper", computerChoice = getComputerChoice())
 });
 
 let scissors = document.querySelector("#scissors");
-scissors.addEventListener('click', function() {
-    let HumanResponse = 'scissors'
-   // return HumanResponse;
-    console.log(HumanResponse)
+scissors.addEventListener('click', () => 
+    {playRound(humanChoice = "scissors", computerChoice = getComputerChoice())
 });
 
-//function getHumanChoice() {
-//    let HumanResponseU = prompt("Rock, Paper or Scissors?");
-//    let HumanResponse = HumanResponseU.toLowerCase();
- //   return HumanResponse;
-//}
+    function playRound() {
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        message.textContent = "You win! Rock SMASHES Scissors!";
+        humanScore++;
+            } else if (humanChoice === "scissors" & computerChoice === "paper") {
+        message.textContent = "You win! Scissors SHREDS Paper!";
+        humanScore++;
+            } else if (humanChoice === "paper" & computerChoice === "rock") {
+        message.textContent = "You win! Paper TOTALLY ENVELOPES Rock!";
+        humanScore++;
+            } else if (computerChoice === "rock" & humanChoice === "scissors") {
+        message.textContent = "You lose! Rock SMASHES Scissors";
+        computerScore++;
+            } else if (computerChoice === "scissors" & humanChoice === "paper") {
+        message.textContent = "You lose! Scissors SHREDS Paper!";
+        computerScore++;
+            } else if (computerChoice === "paper" & humanChoice === "rock") {
+        message.textContent = "You lose! Paper TOTALLY ENVELOPES Rock!";
+        computerScore++;
+            } else {
+        message.textContent = "It's a total tie! Try again."
+            }
+        console.log("Human: " + humanScore);
+        console.log("Computer: " + computerScore);
 
-//   let HumanScore = 0
-//     let ComputerScore = 0
+        hS.textContent = `Human Score = ${humanScore}`;
+        cS.textContent = `Computer Score = ${computerScore}`;
+    };
 
-// function playGame() {
+//Scoreboard 
+let humanScore = 0;
+let computerScore = 0;
 
-//     console.log("Human: " + HumanScore)
-//     console.log("Computer: " + ComputerScore)
-    
-//     function playRound(humanChoice, computerChoice) {
-//     if (humanChoice === "rock" && computerChoice === "scissors") {
-//         console.log("You win! Rock SMASHES Scissors!");
-//         HumanScore++;
-//             } else if (humanChoice === "scissors" & computerChoice === "paper") {
-//         console.log("You win! Scissors SHREDS Paper!");
-//         HumanScore++;
-//             } else if (humanChoice === "paper" & computerChoice === "rock") {
-//         console.log("You win! Paper TOTALLY ENVELOPES Rock!");
-//         HumanScore++;
-//             } else if (computerChoice === "rock" & humanChoice === "scissors") {
-//         console.log("You lose! Rock SMASHES Scissors");
-//         ComputerScore++;
-//             } else if (computerChoice === "scissors" & humanChoice === "paper") {
-//         console.log("You lose! Scissors SHREDS Paper!");
-//         ComputerScore++;
-//             } else if (computerChoice === "paper" & humanChoice === "rock") {
-//         console.log("You lose! Paper TOTALLY ENVELOPES Rock!");
-//         ComputerScore++;
-//             } else {
-//         console.log("It's a total tie! Try again.")
-//             }
-//         console.log("Human: " + HumanScore);
-//         console.log("Computer: " + ComputerScore);
-//     };
-//     playRound(getHumanChoice(), getComputerChoice());
+let divScore = document.querySelector("#score");
 
-//     if (HumanScore > ComputerScore) {
-//         console.log("YOU WIN!")
-//     } else if (HumanScore < ComputerScore) {
-//         console.log("You LOSE!")
-//     } else {
-//         console.log("Tie Game!")
-//     }
-// }
+let hS = document.createElement("p");
+hS.textContent = `Human: = ${humanScore}`;
+
+let cS = document.createElement("p");
+cS.textContent = `Computer: = ${computerScore}`;
+
+divScore.appendChild(hS);
+divScore.appendChild(cS);
+
+console.log("Human: " + humanScore)
+console.log("Computer: " + computerScore)
+
+//Messaging
+let message = document.createElement("p");
+divScore.appendChild(message);
+    // if (HumanScore > ComputerScore) {
+    //     console.log("YOU WIN!")
+    // } else if (HumanScore < ComputerScore) {
+    //     console.log("You LOSE!")
+    // } else {
+    //     console.log("Tie Game!")
+    // }
+
 
 
 
